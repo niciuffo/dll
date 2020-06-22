8f5e1f1b9a967d7cee2420334b817148dcb6b917
Nicolas Realini <nicolas.realini@edu.hefr.ch>
2020-06-22 16:09:11 +0200

    Added medium and big CNN CIFAR-10 NN examples

--- examples/src/cifar10_cnn_big.cpp
+++ examples/src/cifar10_cnn_big.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <chrono>
+#include <dll/neural/conv/conv_same_layer.hpp>
 
 #include "dll/neural/conv/conv_layer.hpp"
 #include "dll/neural/dense/dense_layer.hpp"
@@ -28,13 +29,21 @@ int main(int /*argc*/, char* /*argv*/ []) {
     // Load the dataset
     auto dataset = dll::make_cifar10_dataset(dll::batch_size<256>{}, dll::scale_pre<255>{});
 
+    /*
+     * Option 2: 5x5 Same -> 3x3 Same -> MP -> 5x5 Same -> 3x3 Same -> MP -> 3x3 Same -> 3x3 Same -> MP -> FCNN
+     */
     using dbn_t = dll::dbn_desc<
             dll::dbn_layers<
-                    dll::conv_layer<3, 32, 32, 12, 5, 5, dll::relu>,
-                    dll::mp_3d_layer<12, 28, 28, 1, 2, 2>,
-                    dll::conv_layer<12, 14, 14, 24, 3, 3, dll::relu>,
-                    dll::mp_3d_layer<24, 12, 12, 1, 2, 2>,
-                    dll::dense_layer<24 * 6 * 6, 64, dll::relu>,
+                    dll::conv_same_layer<3, 32, 32, 12, 5, 5, dll::relu>,
+                    dll::conv_same_layer<12, 32, 32, 12, 3, 3, dll::relu>,
+                    dll::mp_3d_layer<12, 32, 32, 1, 2, 2>,
+                    dll::conv_same_layer<12, 16, 16, 24, 5, 5, dll::relu>,
+                    dll::conv_same_layer<24, 16, 16, 24, 3, 3, dll::relu>,
+                    dll::mp_3d_layer<24, 16, 16, 1, 2, 2>,
+                    dll::conv_same_layer<24, 8, 8, 48, 3, 3, dll::relu>,
+                    dll::conv_same_layer<48, 8, 8, 48, 3, 3, dll::relu>,
+                    dll::mp_3d_layer<48, 8, 8, 1, 2, 2>,
+                    dll::dense_layer<48 * 4 * 4, 64, dll::relu>,
                     dll::dense_layer<64, 10, dll::softmax>
             >,
             dll::updater<dll::updater_type::MOMENTUM>,
@@ -56,7 +65,7 @@ int main(int /*argc*/, char* /*argv*/ []) {
     cudaProfilerStart();
     #endif
 
-    dbn->fine_tune(dataset.train(), 20);
+    dbn->fine_tune(dataset.train(), 5);
 
     #ifdef PROFILER
     cudaProfilerStop();
